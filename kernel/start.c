1da33854dd829691a4f9c089ff65fdac9eddb487
Your Name <you@example.com>
2023-09-10 01:47:51 -0500

    save

--- kernel/start.c
+++ kernel/start.c
@@ -3,46 +3,54 @@
 #include "riscv.h"
 #include "memlayout.h"
 
-__attribute__ ((aligned (16))) char stack0[4096 * NCPU];
-
 void main();
 void timerinit();
 
+// entry.S needs one stack per CPU.
+__attribute__ ((aligned (16))) char stack0[4096 * NCPU];
+
+// a scratch area per CPU for machine-mode timer interrupts.
 uint64 timer_scratch[NCPU][5];
 
+// assembly code in kernelvec.S for machine-mode timer interrupt.
 extern void timervec();
 
+// entry.S jumps here in machine mode on stack0.
 void
 start()
 {
-    unsigned long mstatus = r_mstatus();
-    mstatus &= ~MSTATUS_MPP_MASK;
-    mstatus |= MSTATUS_MPP_S;
-    w_mstatus(mstatus);
+  // set M Previous Privilege mode to Supervisor, for mret.
+  unsigned long x = r_mstatus();
+  x &= ~MSTATUS_MPP_MASK;
+  x |= MSTATUS_MPP_S;
+  w_mstatus(x);
 
-    w_mepc((uint64)main);
+  // set M Exception Program Counter to main, for mret.
+  // requires gcc -mcmodel=medany
+  w_mepc((uint64)main);
 
-    w_satp(0);
+  // disable paging for now.
+  w_satp(0);
 
-    // delegate all interrupts and exceptions to supervisor mode.
-    w_medeleg(0xffff);
-    w_mideleg(0xffff);
-    w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
+  // delegate all interrupts and exceptions to supervisor mode.
+  w_medeleg(0xffff);
+  w_mideleg(0xffff);
+  w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
 
-    // configure Physical Memory Protection to give supervisor mode
-    // access to all of physical memory.
-    w_pmpaddr0(0x3fffffffffffffull);
-    w_pmpcfg0(0xf);
+  // configure Physical Memory Protection to give supervisor mode
+  // access to all of physical memory.
+  w_pmpaddr0(0x3fffffffffffffull);
+  w_pmpcfg0(0xf);
 
-    // ask for clock interrupts.
-    timerinit();
+  // ask for clock interrupts.
+  timerinit();
 
-    // keep each CPU's hartid in its tp register, for cpuid().
-    int id = r_mhartid();
-    w_tp(id);
+  // keep each CPU's hartid in its tp register, for cpuid().
+  int id = r_mhartid();
+  w_tp(id);
 
-    // switch to supervisor mode and jump to main().
-    asm volatile("mret");
+  // switch to supervisor mode and jump to main().
+  asm volatile("mret");
 }
 
 // arrange to receive timer interrupts.
@@ -73,10 +81,8 @@ timerinit()
   w_mtvec((uint64)timervec);
 
   // enable machine-mode interrupts.
-//   w_mstatus(r_mstatus() | MSTATUS_MIE);
-  w_mstatus(r_mstatus() & ~MSTATUS_MIE);
+  w_mstatus(r_mstatus() | MSTATUS_MIE);
 
   // enable machine-mode timer interrupts.
-//   w_mie(r_mie() | MIE_MTIE);
-  w_mie(r_mie() & ~MIE_MTIE);
+  w_mie(r_mie() | MIE_MTIE);
 }
